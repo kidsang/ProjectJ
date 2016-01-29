527a437a0a50e4448859d78f811da12a56ed1fd9
kid <963262214@qq.com>
2016-01-29 16:23:26 +0800

    提交一个自动算出所有元素组合的脚本

---
+++ 文档/element_combinations.py
@@ -0,0 +1,25 @@
+﻿# -*- coding: utf-8 -*-
+
+# inputs = ['光', '暗', '金', '木', '水', '火', '土']
+# inputs = ['金', '木', '水', '火', '土']
+inputs = ['光', '暗', '金', '水', '火']
+outputs = []
+
+def pick(inputs, picks, pick_count):
+	if pick_count <= 0:
+		outputs.append(picks)
+		print ''.join(picks).decode('utf-8').encode('gbk')
+		return
+
+	for i in range(len(inputs)):
+		for j in range(pick_count, 0, -1):
+			ele = inputs[i]
+			new_picks = picks[:] + [ele] * j
+			new_inputs = inputs[i+1:]
+			pick(new_inputs, new_picks, pick_count - j)
+
+print '-----------------------------'
+pick(inputs, [], 3)
+print len(outputs)
+
+
